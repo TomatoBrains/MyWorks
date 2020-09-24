369fd9d74a58ed0b6735b9571c48760218fc4914
Valentin <valentin.pav.d@gmail.com>
2020-09-24 17:18:32 +0300

    fix js file in taplab test

--- someWork/testTaplab/src/app/app.js
+++ someWork/testTaplab/src/app/app.js
@@ -1,9 +1,2 @@
+
 import "./app.scss";
-
-import $ from "jquery";
-
-window.jQuery = $;
-
-import "./components/blur";
-import "./components/map";
-import "./components/popup";
--- someWork/testTaplab/src/app/components/blur/index.js
+++
@@ -1,8 +0,0 @@
-let blurRight = document.querySelector(".header_blurRight");
-let blurLeft = document.querySelector(".main__descr_blurLeft");
-window.addEventListener("mousemove", function (e) {
-  let x = e.clientX / window.innerWidth;
-  let y = e.clientY / window.innerHeight;
-  blurRight.style.transform = "translate(-" + x * 15 + "px, -" + y * 15 + "px)";
-  blurLeft.style.transform = "translate(-" + x * 15 + "px, -" + y * 15 + "px)";
-});
