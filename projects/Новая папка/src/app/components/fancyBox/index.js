735e68981eea4791649efa59318629d7bc4eb85f
Valentin <valentin.pav.d@gmail.com>
2020-10-16 00:52:18 +0300

    mobile for  > 500 and > 700

---
+++ projects/Новая папка/src/app/components/fancyBox/index.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+
+window.jQuery = $;
+
+window.$ = window.jQuery = require("jquery");
+
+const fancybox = require("@fancyapps/fancybox");
+
+$('[data-fancybox]').fancybox({
+	arrows: true,
+	thumbs: false,
+	loop: true,
+}); 
