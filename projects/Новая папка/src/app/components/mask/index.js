18fd46f21d5e994ceb7be4f3b022d4b97f73b6e7
Valentin <valentin.pav.d@gmail.com>
2020-10-26 16:08:54 +0300

    dir changes, for projects and learn works use projects, for test works use test works

--- projects/Новая папка/src/app/components/mask/index.js
+++
@@ -1,9 +0,0 @@
-import $ from "jquery";
-
-window.mask = require("jquery.maskedinput/src/jquery.maskedinput.js");
-
-$(function ($) {
-  $(".inputTel").mask("+7 (999) 999-99-99", {
-    placeholder: "+7 (---) ---------",
-  });
-});
